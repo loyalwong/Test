1f003b339ce065a986d6c14bf6be5dfc164d69ca
loyalwong <loyalwong@users.noreply.github.com>
2015-12-30 23:52:04 +0800

    1

--- yuyue.py
+++ yuyue.py
@@ -8,16 +8,17 @@ index_url = "http://yuyue.shdc.org.cn/Main/Default.aspx"
 username = "15921615178"
 password = "87994566"
 #医生
-doctor = "王琛"
+doctor = u"姓名：王琛"
+hospital = u"医院：曙光西院"
 
 def login():
     while b.is_element_present_by_value(u'登 录'):
         print("in the start of loop of login")
         sleep(5)
-        b.fill('txtUserName',username)
-        b.fill('txtPassword',password)
-        sleep(10)
         try:
+            b.fill('txtUserName',username)
+            b.fill('txtPassword',password)
+            sleep(10)
             b.find_by_value(u'登 录').click()
             print("click login button")
         except Exception:
@@ -27,15 +28,8 @@ def login():
             print("already logined")
             break
 
-
-def yuyue():
-    global b
-    b = Browser(driver_name="chrome")
-    b.visit(index_url)
-    login()
-
+def expert_choose():
     try:
-        print("inside try")
         b.click_link_by_text(u'我的专家')
 
         element1 = b.find_by_id('ctl00_ContentPlaceHolder1_divdoctorlist')
@@ -43,14 +37,32 @@ def yuyue():
         for each2 in element2:
             element3 = each2.find_by_css('div.content_doctor_action')
             element4 = each2.find_by_css('div.content_doctor_detail')
-            for each4 in element4:
-                element5 = each4.find_by_css('div.content_doctor_detail_top')
-                for each5 in element5:
-                   element6 = each5.find_by_tag('p')
+            element5 = element4.find_by_css('div.content_doctor_detail_top')
+            element6 = element5.find_by_tag('p')
+            for each6 in element6:
+                if each6.text == doctor:
+                    doctor_flag = TRUE
+                if each6.text == hospital:
+                    hospital_flag = TRUE
+            if doctor_flag == TRUE and hospital_flag == TRUE:
+                element7 = element3.find_by_css('div.content_doctor_action_bottom')
+                break
+
+        button_appointment = element7.find_by_id('btnOrder0')
+        button_appointment.click()
 
     except Exception:
         print("error occur")
 
+def yuyue():
+    global b
+    b = Browser(driver_name="chrome")
+    b.visit(index_url)
+    login()
+
+    expert_choose()
+
+    sleep(10)
     b.quit()
 
 if __name__ == "__main__":
