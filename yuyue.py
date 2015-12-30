efb8e872cbfefa0e27bbbe395e6b93ef5b214c18
loyalwong <loyalwong@users.noreply.github.com>
2015-12-30 23:57:14 +0800

    Update yuyue.py

--- yuyue.py
+++ yuyue.py
@@ -8,16 +8,17 @@ index_url = "http://yuyue.shdc.org.cn/Main/Default.aspx"
 username = "15921615178"
 password = "87994566"
 #医生
-doctor = "王琛"
+doctor = u"姓名：王琛"
+hospital = u"医院：曙光西院"
 
 def login():
     while b.is_element_present_by_value(u'登 录'):
         print("in the start of loop of login")
         sleep(5)
-        b.fill('txtUserName',username)
-        b.fill('txtPassword',password)
-        sleep(10)
         try:
+            b.fill('txtUserName',username)
+            b.fill('txtPassword',password)
+            sleep(10)
             b.find_by_value(u'登 录').click()
             print("click login button")
         except Exception:
@@ -27,6 +28,31 @@ def login():
             print("already logined")
             break
 
+def expert_choose():
+    try:
+        b.click_link_by_text(u'我的专家')
+
+        element1 = b.find_by_id('ctl00_ContentPlaceHolder1_divdoctorlist')
+        element2 = element1.find_by_css('div.content_doctor') + element1.find_by_css('div.content_doctor_even')
+        for each2 in element2:
+            element3 = each2.find_by_css('div.content_doctor_action')
+            element4 = each2.find_by_css('div.content_doctor_detail')
+            element5 = element4.find_by_css('div.content_doctor_detail_top')
+            element6 = element5.find_by_tag('p')
+            for each6 in element6:
+                if each6.text == doctor:
+                    doctor_flag = TRUE
+                if each6.text == hospital:
+                    hospital_flag = TRUE
+            if doctor_flag == TRUE and hospital_flag == TRUE:
+                element7 = element3.find_by_css('div.content_doctor_action_bottom')
+                break
+
+        button_appointment = element7.find_by_id('btnOrder0')
+        button_appointment.click()
+
+    except Exception:
+        print("error occur")
 
 def yuyue():
     global b
@@ -34,35 +60,11 @@ def yuyue():
     b.visit(index_url)
     login()
 
-    try:
-        print("inside try")
-        b.click_link_by_text(u'我的专家')
+    expert_choose()
 
-        element1 = b.find_by_id('ctl00_ContentPlaceHolder1_divdoctorlist')
-        for each1 in element1:
-            element2 = each1.find_by_css('div.content_doctor')
-            for each2 in element2:
-                element3= each2.find_by_css('div.content_doctor_detail_top')
-                for each3 in element3:
-                    try:
-                        element4 = each3.find_by_text(doctor)
-                        print("find specified doctor")
-                        print(element4.value)
-                        print(each3.value)
-                        print(each2.value)
-                        print(each1.value)
-                        break
-                    except Exception:
-                        print("can't find specified doctor")
-
-
-
-    except Exception:
-        print("error occur")
-
-#    w = b.windows[0]
-#    print(w.title)
-#    w.close()
+    sleep(10)
+    b.quit()
 
 if __name__ == "__main__":
     yuyue()
+
